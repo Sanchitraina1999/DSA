4563cf3da072c1e4ea9ef0f8f4642c6779242003
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-10-30 23:14:53 +0530

    10

--- Ass8/8.cpp
+++ Ass8/8.cpp
@@ -8,10 +8,52 @@
 #define MAX 100
 using namespace std;
 class CONVERT{
+int top;
 char infix[MAX], postfix[MAX], stack[MAX];
 public:
-    void push(char);
+    CONVERT(){
+        top=-1;
+    }
     bool isEmpty();
     bool isFull();
-    
+    void push(char);
+    char pop();
+    void read();
+    void infixToPostfix();
+    int prior(char);
+};
+
+bool CONVERT::isEmpty(){
+    if(top==-1)
+        return 1;
+    else
+        return 0;    
+}
+
+bool CONVERT::isFull(){
+    if(top==MAX-1)
+        return 1;
+    else
+        return 0;
+}
+
+void CONVERT::push(char symbol){
+    if(isFull())
+        cout<<"\nOverflow";
+    else
+        stack[++top]=symbol;
+}
+
+int main(){
+    char choice;
+    CONVERT c;
+up:
+    c.read();
+    c.infixToPostfix();
+    cout<<"\nDo you want to continue? (y/n) : ";
+    cin>>choice;
+    if(choice=='y')
+        goto up;
+    else
+        return 0;
 }
