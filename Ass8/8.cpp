df7cf5744c6b5f3a628ac6a5379e3cc0b34b8a58
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-10-30 19:49:15 +0530

    10

--- Ass8/8.cpp
+++ Ass8/8.cpp
@@ -4,146 +4,91 @@
  character, Input Postfix expression must be in a desired format, Only '+', '-', '*'
  and '/ ' operators are expected.
  */
-
-#include <iostream>
+#include<iostream>
+#define MAX 100
 using namespace std;
-class stack
-{
-public:
-    char stack_array[50];
+class STACK{
     int top;
-    stack()
-    {
-        top = -1;
-    }
-    void push(char symbol)
-    {
-        if (full())
-            cout << "\nStack overflow:\n";
-        else
-        {
-            top++;
-            stack_array[top] = symbol;
-        }
-    }
-    char pop()
-    {
-        if (empty())
-            return ('#'); // Return value '#' indicates stack is empty
-        else
-            return (stack_array[top--]);
-    }
-    int empty()
-    {
-        if (top == -1)
-            return (1);
-        else
-            return (0);
-    }
-    int full()
-    {
-        if (top == 49)
-            return (1);
-        else
-            return (0);
-    }
-
-private:
-    char infix[50];
-    char postfix[50];
-
+    char infix[MAX], postfix[MAX], stackArray[MAX];
 public:
-    void read()
-    {
-        cout << "\nEnter an infix expression:";
-        cin >> infix;
+    STACK(){
+        top=-1;
     }
-    int white_space(char symbol)
-    {
-        if (symbol == ' ' || symbol == '\t' || symbol == '\0')
-            return 1;
-        else
-            return 0;
-    }
-    void ConvertToPostfix()
-    {
-        int entry, p;
-        char prev;
-        p = 0;
-        for (int i = 0; infix[i] != '\0'; i++)
-        {
-            if (!white_space(infix[i]))
-            {
-                switch (infix[i])
-                {
-                case '(':
-                    push(infix[i]);
-                    break;
-                case ')':
-                    while ((prev = pop()) != '(')
-                        postfix[p++] = prev;
-                    break;
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                    if (!empty())
-                    {
-                        entry = prior(infix[i]);
-                        prev = pop();
-                        while (entry <= prior(prev))
-                        {
-                            postfix[p++] = prev;
-                            if (!empty())
-                                prev = pop();
-                            else
-                                break;
-                        }
-                        if (entry > prior(prev))
-                            push(prev);
-                    }
-                    push(infix[i]);
-                    break;
-                default:
-                    postfix[p++] = infix[i];
-                    break;
-                }
-            }
-        }
-        while (!empty()) //while stack is not empty
-            postfix[p++] = pop();
-        postfix[p] = '\0';
-        cout << "\nThe postfix expression is: " << postfix << endl;
-    }
-    int prior(char symbol)
-    {
-        switch (symbol)
-        {
-        case '/':
-            return (4);
-        case '*':
-            return (3);
-        case '+':
-            return (2);
-        case '-':
-            return (1);
-        case '(':
-            return (0);
-        default:
-            return (-1);
-        }
-    }
+    void read();
+    bool isEmpty();
+    bool isFull();
+    void push(char);
+    void infixToPostfix();
+    char pop();
+    int prior(char);
 };
-int main()
-{
-    char choice = 'y';
-    stack expr;
-    while (choice == 'y')
-    {
-        expr.read();
-        expr.ConvertToPostfix();
-        cout << "\n\nDo you want to continue ? (y/n): ";
-        cin >> choice;
+
+void STACK::read(){
+    cout<<"\nEnter an infix expression: ";
+    cin>>infix;
+}
+
+bool STACK::isEmpty(){
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
+
+bool STACK::isFull(){
+    if(top==MAX-1)
+        return 1;
+    else
+        return 0;
+}
+
+void STACK::push(char symbol){
+    if(!isFull())
+        cin>>stackArray[top++];
+    else
+        cout<<"\nOverflow";  
+}
+
+char STACK::pop(){
+    if(!isEmpty())
+        return stackArray[top--];
+    else
+        cout<<"\nUnderflow";    
+}
+
+void STACK::infixToPostfix(){
+    for(int i=0;infix[i]!='\0';i++){
+        
     }
-    return 0;
+}
+
+int STACK::prior(char symbol){
+    switch(symbol){
+        case '/':
+            return 4;
+        case '*':
+            return 3;
+        case '+':
+            return 2;
+        case '-':
+            return 1;
+        case '(':
+            return 0;
+        default:
+            return -1;
+    }
+}
+
+int main(){
+    STACK s;
+    char choice;
+up: 
+    s.read();
+    s.infixToPostfix();
+    cout<<"\nDo you want to continue? (y/n) : ";
+    cin>>choice;
+    if(choice=='y' || choice=='Y')
+        goto up;
+    else
+        return 0;
 }
