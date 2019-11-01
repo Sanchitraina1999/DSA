f337827036df2daa9106b3b47fcafd5e1f90c16a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-01 14:57:20 +0530

    12

--- Ass6/6.cpp
+++ Ass6/6.cpp
@@ -1,21 +1,38 @@
 /*
 Write a C++ program to realize polynomial equation using circular link list and perform following operation
-. To input &output polynomials 
+. To input & output polynomials 
 . Evaluates a polynomial at given value of x 
 . Add two polynomials. 
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class node{
-
+class node
+{
+public:
+    int value;
+    int power;
+    node *next;
 };
 
-class list{
+class list
+{
+    node *head, *tail;
 
+public:
+    list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
 };
 
-int main(){
+int main()
+{
+    cout << "\nPolynomial Equation using Circular Linked List";
+    cout << "\n1. Input polynomial equation";
+    cout << "\n2. Output polynomial equation";
+    cout << "\n3. Equalate the polynomial equation for a given value of x";
 
     return 0;
 }
