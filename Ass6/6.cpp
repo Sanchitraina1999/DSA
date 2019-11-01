274e7b020932328b4eb387c7c76a0a359aadd639
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-01 13:39:24 +0530

    12

---
+++ Ass6/6.cpp
@@ -0,0 +1,6 @@
+/*
+Write a C++ program to realize polynomial equation using circular link list and perform following operation
+. To input &output polynomials 
+. Evaluates a polynomial at given value of x 
+. Add two polynomials. 
+*/
