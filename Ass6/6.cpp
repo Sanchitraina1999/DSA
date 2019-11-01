23b25ccf1002d6491852f17b132e621ba09a38b1
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-01 15:09:28 +0530

    12

--- Ass6/6.cpp
+++ Ass6/6.cpp
@@ -15,24 +15,68 @@ public:
     node *next;
 };
 
-class list
+class polynomial
 {
     node *head, *tail;
 
 public:
-    list()
+    polynomial()
     {
         head = NULL;
         tail = NULL;
     }
+    void input();
+    void output();
+    void eval(int);
+    void add(polynomial &);
 };
+
+void polynomial::input(){
+    
+}
+
+void polynomial:: output(){
+
+}
+
+void polynomial::eval(int){
+
+}
+
+void polynomial::add(polynomial &){
+
+}
 
 int main()
 {
+    polynomial p1, p2, *p;
+    int choice1,choice2;
+up:
     cout << "\nPolynomial Equation using Circular Linked List";
+    cout << "Equation 1";
+    cout << "Equation 2";
+    cout<<"\n Enter your choice : ";
+    cin>>choice1;
+    if(choice1==1)
+        p=&p1;
+    else if(choice1==2)
+        p=&p2;
+    else
+        cout<<"\nNo such option";
+down:    
     cout << "\n1. Input polynomial equation";
     cout << "\n2. Output polynomial equation";
-    cout << "\n3. Equalate the polynomial equation for a given value of x";
-
+    cout << "\n3. Evaluate the polynomial equation for a given value of x";
+    cout << "\n4. Add two polynomial equation";
+    cin >> choice2;
+    switch (choice2)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
     return 0;
 }
