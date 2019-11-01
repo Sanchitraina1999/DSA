c635e8c06db416b856c790732fc024ae57f0b377
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-01 16:01:33 +0530

    12

--- Ass6/6.cpp
+++ Ass6/6.cpp
@@ -93,56 +93,59 @@ int main()
     int choice1, choice2, x;
     char yn = 'y';
 up:
-    while (yn == 'y')
+    cout << "\nPolynomial Equation using Circular Linked List";
+    cout << "\nEquation 1";
+    cout << "\nEquation 2";
+    cout << "\n Enter your choice : ";
+    cin >> choice1;
+    if (choice1 == 1)
+        p = &p1;
+    else if (choice1 == 2)
+        p = &p2;
+    else
+        cout << "\nNo such option";
+down:
+    cout << "\n1. Input polynomial equation";
+    cout << "\n2. Output polynomial equation";
+    cout << "\n3. Evaluate the polynomial equation for a given value of x";
+    if (choice1 == 1)
+        cout << endl;
+    if (choice1 == 2)
+        cout << "\n4. Add two polynomial equation\n";
+    cin >> choice2;
+    switch (choice2)
     {
-        cout << "\nPolynomial Equation using Circular Linked List";
-        cout << "\nEquation 1";
-        cout << "\nEquation 2";
-        cout << "\n Enter your choice : ";
-        cin >> choice1;
-        if (choice1 == 1)
-            p = &p1;
-        else if (choice1 == 2)
-            p = &p2;
+    case 1:
+        cout << "Enter the degree of the polynomial";
+        cin >> p->degree;
+        p->input(p->degree);
+        break;
+    case 2:
+        if (p->degree == 0)
+            cout << "\nEquation is not inputed";
         else
-            cout << "\nNo such option";
-    down:
-        cout << "\n1. Input polynomial equation";
-        cout << "\n2. Output polynomial equation";
-        cout << "\n3. Evaluate the polynomial equation for a given value of x";
-        if (choice1 == 1)
-            cout << endl;
-        if (choice1 == 2)
-            cout << "\n4. Add two polynomial equation\n";
-        cin >> choice2;
-        switch (choice2)
-        {
-        case 1:
-            cout << "Enter the degree of the polynomial";
-            cin >> p->degree;
-            p->input(p->degree);
-            break;
-        case 2:
-            if (p->degree == 0)
-                cout << "\nEquation is not inputed";
-            else
-                p->output();
-            break;
-        case 3:
-            cout << "\nEnter a value of x for which you want to evaluate the polynomial ";
-            cin >> x;
-            p->eval(x);
-            break;
-        case 4:
-            p2.add(p1);
-            break;
-        case 5:
-            yn = 'n';
-            break;
-        default:
-            cout << "\nNo such option";
-            break;
-        }
+            p->output();
+        break;
+    case 3:
+        cout << "\nEnter a value of x for which you want to evaluate the polynomial ";
+        cin >> x;
+        p->eval(x);
+        break;
+    case 4:
+        p2.add(p1);
+        break;
+    case 5:
+        yn = 'n';
+        break;
+    default:
+        cout << "\nNo such option";
+        break;
     }
+    cout<<"\nDo you want to continue (y/n) : ";
+    cin>>yn;
+    if(yn=='y')
+        goto down;
+    else
+        goto up;    
     return 0;
 }
