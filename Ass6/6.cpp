22c66285b6309ac5661b78296e88d0395356d6d0
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-01 15:17:38 +0530

    12

--- Ass6/6.cpp
+++ Ass6/6.cpp
@@ -32,7 +32,7 @@ public:
 };
 
 void polynomial::input(){
-    
+
 }
 
 void polynomial:: output(){
@@ -72,7 +72,7 @@ down:
     switch (choice2)
     {
     case 1:
-        
+        p->input();
         break;
     
     default:
