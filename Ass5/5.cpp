79e7a3c922e5c725048efa6c0710053a81328457
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-11-02 13:09:09 +0530

    13

--- Ass5/5.cpp
+++ Ass5/5.cpp
@@ -8,3 +8,54 @@ On demand
 . The booking can be cancelled.
 */
 #include<iostream>
+using namespace std;
+
+class node{
+    int booking;
+    node *next, *prev;
+};
+
+class TICKET{
+    node *head,*tail;
+public:
+    TICKET()
+    {
+        head=tail=NULL;
+    }
+    void available();
+    void book();
+    void cancel();
+};
+
+
+
+int main(){
+    TICKET t;
+    int choice;
+    char yn;
+up:
+    cout<<"\nTicket Booking System of Cinemax Theater";
+    cout<<"\n1. The list of available seats";
+    cout<<"\n2. Book seats";
+    cout<<"\n3. Cancel seat booking";
+    cin>>choice;
+    switch(choice){
+        case 1:
+            t.available();
+            break;
+        case 2:
+            t.book();
+            break;
+        case 3: 
+            t.cancel();
+            break;
+        default:
+            cout<<"\nNo such choice";
+   }
+   cout<<"\nDo you want to continue (y/n) : ";
+   cin>>yn;
+   if(yn=='y')
+        goto up;
+   else
+        return 0;
+}
